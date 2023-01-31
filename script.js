cbf64fa2f07b99a7ce8af767dca0e32439f38246
keginold philip <xkegix@users.noreply.github.com>
2023-01-31 17:26:07 +0000

    everything working and space used well, footer getting stuck when scrolling fixed

---
+++ script.js
@@ -0,0 +1,12 @@
+// const button = document.querySelector("button");
+
+// button.addEventListener("mousedown", (event) => {
+//   button.style.transition = "none";
+//   button.style.backgroundImage = `linear-gradient(to right, 
+//                                     red, orange, yellow, green, blue, indigo, violet)`;
+// });
+
+// button.addEventListener("mouseup", (event) => {
+//   button.style.transition = "background-image 1s linear";
+//   button.style.backgroundImage = "none";
+// });
